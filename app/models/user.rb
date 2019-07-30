ab2835c7f9409462b4fbbcb06d1c4eb89f812dde
SwanHub <jdprince555@gmail.com>
2019-07-29 20:35:15 -0600

    outlining some methods, added cli.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,44 @@ class User < ActiveRecord::Base
   has_many :battles, through: :adventurers
   has_many :enemies, through: :adventurers
 
+  def create_adventurer
+    # prompt1: what would you like the character name to be?
+    ### save input
+    # prompt2: do you want your character to have a battle cry?
+    ### save input
+
+    # Adventurer.create(name: prompt1, battlecry: prompt2)
+
+  end
+
+  def user_stats
+    # fastest victory
+    # treks completed
+    # total successful treks
+    # total unsuccessulf treks
+  end
+
+  def trek_details
+    # character stats
+    # enemies battled
+    # items accumulated
+    # currency used
+  end
+
+  def self.leaderboards
+    # fastest victory
+    # treks completed
+    # successful treks
+    # unsuccessulf treks
+    # richest final character form
+    # healthiest final character form
+    # most savage final character form
+    # luckiest final character form
+  end
+
+  def self.all_adventurers
+    # a list of all adventurers created and who played with them.
+  end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,11 @@ require 'pry'
 connection = ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/trek.db')
 # eliminates message logging to terminal.
 # ActiveRecord::Base.logger = nil
-prompt = TTY::Prompt.new
+# prompt = TTY::Prompt.new
 
 jackson = User.find_or_create_by(name: "Jackson")
 dan = User.find_or_create_by(name: "Dan")
 
 binding.pry
+
 # play_game
